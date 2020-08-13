74ced36e410f6fbb49da112621ec29c03afa64f4
Daniel Peixoto <danielspeixoto@Daniels-MacBook-Pro.local>
2020-08-12 21:43:52 -0300

    fdsf

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,3 @@ RUN pipenv lock --requirements > requirements.txt
 RUN pip install -r requirements.txt
 ADD . /project
 CMD NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program python3 wsgi.py
-EXPOSE 5000
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,9 @@
+import os
+
 from app.main import app
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = os.getenv('PORT')
+    if port is None:
+        port = 5000
+    app.run(host="0.0.0.0", port=port)
